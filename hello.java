24a98a48c4766e5b27d361eb80a1f93d5b4b2fba
Chitra Srivastava <chitra.sri23.7.03@gmail.com>
2024-08-04 18:14:26 +0530

    my first java pro

---
+++ hello.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class myfirst
+{
+   public static void main(string arg[])
+   {
+     system.out.println("hello world");
+	 }
+}
---
+++ myfirst.java
@@ -0,0 +1,9 @@
+
+
+class myfirst
+{
+   public static void main(string arg[])
+   {
+     System.out.print("hello world");
+	 }
+}
